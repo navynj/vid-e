e104d92a17c56cd3036b6e1aa1068dc779590cc6
김진아 <gimjin-a@gimjin-aui-MacBookPro.local>
2021-03-10 17:29:01 +0900

    push

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+#from . import cutting
+
+UPLOAD_FOLDER = '/Users/gimjin-a/Desktop/noise/static/upload/'
+ALLOWED_EXTENSIONS = {'mp4', 'wav'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#파일 업로드페이지 렌더링
+@app.route('/')
+#@app.route('/index')
+def index():
+    return render_template("index.html")
+
+#파일 업로드
+@app.route('/upload', methods = ['GET','POST'])
+def upload_file():
+    if request.method == "POST":
+        #파일객체 가져오기
+        f = request.files['mp4file']
+        filename = secure_filename(f.filename)
+        f.save(UPLOAD_FOLDER + filename)
+        #업로드하면 cutting페이지 나오고 업로드한 파일 재생할 수 있게끔 했다
+        return render_template("cut.html", output = "upload/"+filename)
+    #파일업로드하고나서 cut sound 선택할 수 있게 하는게 나을라나?
+
+#무음자르기 페이지 렌더링
+@app.route('/cut')
+def cut():
+    return render_template("cut.html", title = "cut")
+
+#추후 효과음추가 페이지 렌더링
+@app.route('/sound')
+def sound():
+    return render_template("sound.html", title = "sound")
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
---
+++ cutting.py
@@ -0,0 +1,48 @@
+import moviepy.editor as mp
+import librosa
+import librosa.display
+import numpy as np
+import IPython.display
+import matplotlib.pyplot as plt
+
+INPUT = "/static/upload/"
+fnames = os.listdir(INPUT)
+
+# 1. Audio extract from video
+def extract_wav(fnames):
+    clip = mp.VideoFileClip(f"{INPUT}/{fname}") for fname in fnames]
+for i, clip in enumerate(clips):
+  clip.audio.write_audiofile(audio_inputs[i])
+
+
+def load_audio(audio_path):
+  y, sr = librosa.core.load(audio_path, sr=None)
+  return y, sr
+
+
+"""def show_plt(signal)
+    plt.figure(figsize=(8,4))
+    librosa.display.waveplot(signal, 44100 , alpha = 0.5)
+    plt.xlabel("time(s)")
+    plt.grid()
+    plt.show()
+
+def get_non_mute(y, tdb):
+    non_mute_intervals = librosa.effects.split(y, top_db=tdb)
+    non_mute_audio = [y[i[0]:i[1]] for i in non_mute_intervals]
+    non_mute_audio = np.concatenate(non_mute_audio)
+    IPython.display.display(IPython.display.Audio(data=non_mute_audio, rate=sr))
+    return non_mute_intervals, non_mute_audio
+
+def remove_silence(sr, non_mute_intervals):
+      non_mute_clips = [clips[index].subclip(i[0]/sr, i[1]/sr) for i in non_mute_intervals]
+      final_clip = mp.concatenate_videoclips(non_mute_clips)
+      final_clip.write_videofile(f"{OUTPUT}/{fnames[index].split('.')[0]}_OUTPUT.mp4")
+      shutil.move(f"{INPUT}/{fnames[index]}", f"{COMPLETE}/{fnames[index]}")
+      return final_clip, audio_inputs[:index] + audio_inputs[index+1:]
+
+      top_db = get_num("top_db")
+        
+non_mute_intervals, non_mute_audio = get_non_mute(y, top_db)
+
+final_clip, audio_inputs = remove_silence(sr, non_mute_intervals)"""
