c7782183f688c62794be8f287bc7f14080b01bb4
kim-jin-a <jina1190@hanyang.ac.kr>
2021-05-11 15:03:05 +0900

    1

---
+++ ptsd_lecture.js
@@ -0,0 +1,510 @@
+export default[
+    {
+    "video": {
+        "name": "ptsd_lecture.mp4",
+        "id": "ptsd_lecture",
+        "ext": "mp4",
+        "path": "/Users/lyoon/0lyoonj/projects/summit_capstone/remove_silence/static/temp/ptsd_lecture.mp4"
+    },
+    "audio": {
+        "name": "ptsd_lecture.wav",
+        "path": "/Users/lyoon/0lyoonj/projects/summit_capstone/remove_silence/static/temp/ptsd_lecture.wav",
+        "gcs_uri": "gs://temp-bucket-stteff-0411/ptsd_lecture.wav"
+    },
+    "keyword_sentences": [
+        {
+            "time": {
+                "sentence": {
+                    "start": 500,
+                    "end": 3300
+                },
+                "keyword": {
+                    "start": 1300,
+                    "end": 1800
+                }
+            },
+            "text": {
+                "prev": "어",
+                "keyword": "그리고",
+                "next": "이제 약 처방"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 7700,
+                    "end": 12000
+                },
+                "keyword": {
+                    "start": 9300,
+                    "end": 10400
+                }
+            },
+            "text": {
+                "prev": "알아요 돈 스스로가",
+                "keyword": "그런데",
+                "next": "이제 죽은 친구들에"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 17900,
+                    "end": 21800
+                },
+                "keyword": {
+                    "start": 19000,
+                    "end": 19700
+                }
+            },
+            "text": {
+                "prev": "하는 거 이거는",
+                "keyword": "그",
+                "next": "친구들에 대한 충성심이"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 22300,
+                    "end": 25700
+                },
+                "keyword": {
+                    "start": 23600,
+                    "end": 24000
+                }
+            },
+            "text": {
+                "prev": "배신하는 거야 이런",
+                "keyword": "어떤",
+                "next": "생각을 가지고 있다"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 25700,
+                    "end": 29200
+                },
+                "keyword": {
+                    "start": 27000,
+                    "end": 27300
+                }
+            },
+            "text": {
+                "prev": "하는 거지요 사고가",
+                "keyword": "굉장히",
+                "next": "많이 외국 되지요"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 25900,
+                    "end": 29400
+                },
+                "keyword": {
+                    "start": 27300,
+                    "end": 27600
+                }
+            },
+            "text": {
+                "prev": "거지요 사고가 굉장히",
+                "keyword": "많이",
+                "next": "외국 되지요 아주"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 27300,
+                    "end": 31000
+                },
+                "keyword": {
+                    "start": 28400,
+                    "end": 29200
+                }
+            },
+            "text": {
+                "prev": "많이 외국 되지요",
+                "keyword": "아주",
+                "next": "사소한 일에 실망하거나"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 52600,
+                    "end": 56600
+                },
+                "keyword": {
+                    "start": 53700,
+                    "end": 54700
+                }
+            },
+            "text": {
+                "prev": "하는 친구가 있는데요",
+                "keyword": "정말",
+                "next": "친하게 지냈으면 인생의"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 70000,
+                    "end": 76300
+                },
+                "keyword": {
+                    "start": 72900,
+                    "end": 73500
+                }
+            },
+            "text": {
+                "prev": "어느날 톰은 하시는",
+                "keyword": "그",
+                "next": "시각에 초대를 이끌고"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 75200,
+                    "end": 80200
+                },
+                "keyword": {
+                    "start": 77000,
+                    "end": 77400
+                }
+            },
+            "text": {
+                "prev": "가득 잘하고 있는",
+                "keyword": "그",
+                "next": "돈으로 도보 정차를"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 87100,
+                    "end": 91600
+                },
+                "keyword": {
+                    "start": 88500,
+                    "end": 89100
+                }
+            },
+            "text": {
+                "prev": "톤 가까이 있던",
+                "keyword": "그",
+                "next": "장병들이 하나둘 청와대"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 92400,
+                    "end": 97900
+                },
+                "keyword": {
+                    "start": 94200,
+                    "end": 95300
+                }
+            },
+            "text": {
+                "prev": "내 옆에 있던",
+                "keyword": "정말",
+                "next": "그 소중한 그"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 93200,
+                    "end": 98300
+                },
+                "keyword": {
+                    "start": 95300,
+                    "end": 95500
+                }
+            },
+            "text": {
+                "prev": "옆에 있던 정말",
+                "keyword": "그",
+                "next": "소중한 그 동료들이"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 94200,
+                    "end": 101900
+                },
+                "keyword": {
+                    "start": 96200,
+                    "end": 96900
+                }
+            },
+            "text": {
+                "prev": "정말 그 소중한",
+                "keyword": "그",
+                "next": "동료들이 하나둘 생각을"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 101800,
+                    "end": 105800
+                },
+                "keyword": {
+                    "start": 102400,
+                    "end": 102900
+                }
+            },
+            "text": {
+                "prev": "해 보세요 얼마나",
+                "keyword": "그",
+                "next": "공포스럽고 정말 가슴이"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 102100,
+                    "end": 106800
+                },
+                "keyword": {
+                    "start": 104300,
+                    "end": 104900
+                }
+            },
+            "text": {
+                "prev": "얼마나 그 공포스럽고",
+                "keyword": "정말",
+                "next": "가슴이 찢어지게 찌요"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 105800,
+                    "end": 109800
+                },
+                "keyword": {
+                    "start": 107400,
+                    "end": 107900
+                }
+            },
+            "text": {
+                "prev": "찌요 톰은 머릿속에서",
+                "keyword": "절대",
+                "next": "지워지지 않는 모습을"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 109100,
+                    "end": 112400
+                },
+                "keyword": {
+                    "start": 110300,
+                    "end": 110600
+                }
+            },
+            "text": {
+                "prev": "보게 된 거죠",
+                "keyword": "단",
+                "next": "몇 초만에 소대"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 114600,
+                    "end": 118500
+                },
+                "keyword": {
+                    "start": 116100,
+                    "end": 116800
+                }
+            },
+            "text": {
+                "prev": "광경을 목격하게 됩니다",
+                "keyword": "특히",
+                "next": "가장 친한 그"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 116100,
+                    "end": 121200
+                },
+                "keyword": {
+                    "start": 117600,
+                    "end": 117900
+                }
+            },
+            "text": {
+                "prev": "특히 가장 친한",
+                "keyword": "그",
+                "next": "알렉스의 발이 허공에"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 126400,
+                    "end": 130199
+                },
+                "keyword": {
+                    "start": 127200,
+                    "end": 127500
+                }
+            },
+            "text": {
+                "prev": "보게 된 거죠",
+                "keyword": "그러니까",
+                "next": "그 꼭 내가"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 126700,
+                    "end": 130800
+                },
+                "keyword": {
+                    "start": 127500,
+                    "end": 128500
+                }
+            },
+            "text": {
+                "prev": "된 거죠 그러니까",
+                "keyword": "그",
+                "next": "꼭 내가 경험한"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 126900,
+                    "end": 131300
+                },
+                "keyword": {
+                    "start": 128500,
+                    "end": 129400
+                }
+            },
+            "text": {
+                "prev": "거죠 그러니까 그",
+                "keyword": "꼭",
+                "next": "내가 경험한 거"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 134900,
+                    "end": 140800
+                },
+                "keyword": {
+                    "start": 137400,
+                    "end": 138700
+                }
+            },
+            "text": {
+                "prev": "평생 유일하게 친한",
+                "keyword": "진짜",
+                "next": "친구였어요 진짜 지금"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 136100,
+                    "end": 144400
+                },
+                "keyword": {
+                    "start": 139500,
+                    "end": 140100
+                }
+            },
+            "text": {
+                "prev": "친한 진짜 친구였어요",
+                "keyword": "진짜",
+                "next": "지금 유일한 친구"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 140100,
+                    "end": 146100
+                },
+                "keyword": {
+                    "start": 141300,
+                    "end": 144400
+                }
+            },
+            "text": {
+                "prev": "지금 유일한 친구",
+                "keyword": "그래서",
+                "next": "안 받아 부하들이"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 147700,
+                    "end": 152400
+                },
+                "keyword": {
+                    "start": 149100,
+                    "end": 149300
+                }
+            },
+            "text": {
+                "prev": "꼬꾸라져 그들의 몸이",
+                "keyword": "계속",
+                "next": "인제 보이게됩니다 매복하는"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 152800,
+                    "end": 157700
+                },
+                "keyword": {
+                    "start": 154000,
+                    "end": 154900
+                }
+            },
+            "text": {
+                "prev": "하는 소리를 하든지",
+                "keyword": "또는",
+                "next": "냄새 라든지 모습과"
+            }
+        },
+        {
+            "time": {
+                "sentence": {
+                    "start": 155300,
+                    "end": 159300
+                },
+                "keyword": {
+                    "start": 157700,
+                    "end": 158100
+                }
+            },
+            "text": {
+                "prev": "라든지 모습과 마주치기라도",
+                "keyword": "그래서",
+                "next": "소리 냄새 이런"
+            }
+        }
+    ]
+}
+];
