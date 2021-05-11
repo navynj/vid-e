fd402f9ab04855e2570bb9e49763147f64c1df90
0lyoonj <0lyoonj@gmail.com>
2021-05-12 07:42:48 +0900

    Update templates : 파일명 함수명 변경

--- project/static/js/video.js
+++ project/static/js/video.js
@@ -6,3 +6,52 @@ const getEvent = (targetId) => {
         updateComplete(targetId, data.src);
     }, false);
 }
+
+// status별 상태 업데이트 : 추후 경우별 수정
+const rmSilenceUpdate = (status) => {
+    const id = 'rm-silence';
+    switch (status) {
+        case 'PROCESS':
+            updateProcess(id);
+            getEvent(id);
+        case 'COMPLETE':
+            updateComplete(id);
+        case 'READY':
+            updateReady(id);
+            // 건너뛰기 버튼
+        case 'DISABLED':
+            updateComplete(id);
+            updateDisabled(id);
+        default:
+            console.log(id + ' : no valid status');
+    }
+}
+
+const addEffectUpdate = (status) => {
+    const id = 'add-effect';
+    switch (status) {
+        case 'PROCESS':
+            updateProcess(id);
+            getEvent(id);
+        case 'COMPLETE':
+            updateComplete(id);
+        case 'READY':
+            updateReady(id);
+        case 'DISABLED':
+            updateReady(id);
+            updateDisabled(id);
+        default:
+            console.log(id + ' : no valid status');
+}
+
+
+// 
+function updateComplete(targetId, src) {
+    const target = document.getElementById(targetId);
+    const video = target.querySelector('video');
+    video.src = src;
+    video.classList.remove('hide');
+}
+function updateReady(targetId) {;}
+function updateProcess(targetId) {;}
+function updateDisabled(targetId) {;}
