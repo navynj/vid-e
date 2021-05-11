ec3e044504a90219e4a37167df7a292058a7dec8
0lyoonj <0lyoonj@gmail.com>
2021-05-12 07:38:27 +0900

    Update templates : id 명명 규칙 hypen으로 통일

--- project/static/js/rm_silence.js
+++ project/static/js/rm_silence.js
@@ -3,24 +3,24 @@
 // ================
 const create_radio = (tdb) => {
     // 동일값 존재하면 false 반환
-    if (document.getElementById(`${tdb}_radio`))
+    if (document.getElementById(`${tdb}-radio`))
         return false;
     // 라디오 정의 [value: tdb, display: none]
     const radio = document.createElement('input');
     radio.type = "radio";
     radio.name = "tdb";
-    radio.id = `${tdb}_radio`;
+    radio.id = `${tdb}-radio`;
     radio.value = tdb;
     radio.checked = false;
     radio.classList.add("hide");
     // 라벨 정의 [click : update_waveform]
     const label = document.createElement('label');
-    label.id = `${tdb}_label`;
+    label.id = `${tdb}-label`;
     label.htmlFor = radio.id;
     label.innerText = tdb;
     label.classList.add("processing");
     // 폼에 현재 라디오+라벨 추가
-    const form = document.getElementById('split_outputs');
+    const form = document.getElementById('split-outputs');
     form.appendChild(radio);
     form.appendChild(label);
     return true;
@@ -33,13 +33,13 @@ const update_radio = (tdb) => {
       if(radios[i].checked)
         radios[i].checked = false;
     // 현재 라디오 선택 처리
-    const radio = document.getElementById(`${tdb}_radio`);
+    const radio = document.getElementById(`${tdb}-radio`);
     radio.checked = true;
 }
 
 const update_label = (tdb, sr, intervals) => {
     // 라벨 클래스 업데이트
-    const label = document.getElementById(`${tdb}_label`);
+    const label = document.getElementById(`${tdb}-label`);
     label.classList.remove("processing");
     label.classList.add("complete");
     // // waveform 업데이트
@@ -56,7 +56,7 @@ const post_json = (json_data) => {
 }
 
 const tdb_split = url => {
-    const tdb = document.getElementById("topdb_input").value;
+    const tdb = document.getElementById("tdb-input").value;
     if (create_radio(tdb)){
         fetch( url, post_json({ 'tdb' : tdb }) )
         .then( res => { return res.json() } )
@@ -76,7 +76,7 @@ const export_result = url => {
     fetch( url, { 
         method: 'POST',
         headers: { 'Content-Type' : 'application/json' },
-        body : { 'tdb' : document.getElementById("topdb_input").value }
+        body : { 'tdb' : document.getElementById("tdb-input").value }
     })
     .then( res => { return res.json() } )
     .then( data => { console.log(data) } )
