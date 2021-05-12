17db3714abc4a34d7941fc6d34ab5bca84b3db21
0lyoonj <0lyoonj@gmail.com>
2021-05-12 09:02:30 +0900

    Add : export 응답 SSE 처리 (Front)

--- project/static/js/video.js
+++ project/static/js/video.js
@@ -1,58 +1,86 @@
 // sse 이벤트
-const getEvent = (targetId) => {
+const getEvent = (target) => {
     var es = new EventSource('/stream');
     es.addEventListener('COMPLETE', (event) => {
         const data = JSON.parse(event.data);
-        updateComplete(targetId, data.src);
+        updateComplete(target, data.src);
     }, false);
 }
 
 // status별 상태 업데이트 : 추후 경우별 수정
-const rmSilenceUpdate = (status) => {
-    const id = 'rm-silence';
+const updateStatus = (id, status) => {
+    const target = getElementById(id);
     switch (status) {
         case 'PROCESS':
-            updateProcess(id);
-            getEvent(id);
+            updateProcess(target);
+            getEvent(target);
+            break;
         case 'COMPLETE':
-            updateComplete(id);
+            updateComplete(target);
+            break;
         case 'READY':
-            updateReady(id);
-            // 건너뛰기 버튼
+            updateReady(target);
+            break;
         case 'DISABLED':
-            updateComplete(id);
-            updateDisabled(id);
+            target.classList.add('disabled');
+            break;
         default:
             console.log(id + ' : no valid status');
     }
 }
+const rmSilenceUpdate = (status) => updateStatus('rm-silence', status);
+const addEffectUpdate = (status) => updateStatus('add-effect', status);
 
-const addEffectUpdate = (status) => {
-    const id = 'add-effect';
-    switch (status) {
-        case 'PROCESS':
-            updateProcess(id);
-            getEvent(id);
-        case 'COMPLETE':
-            updateComplete(id);
-        case 'READY':
-            updateReady(id);
-        case 'DISABLED':
-            updateReady(id);
-            updateDisabled(id);
-        default:
-            console.log(id + ' : no valid status');
-    }
-}
-
-
-// 
-function updateComplete(targetId, src) {
-    const target = document.getElementById(targetId);
-    const video = target.querySelector('video');
+// 상태별 html 업데이트
+function updateComplete(target, src) {
+    // video
+    const video = target.querySelector('.placeholder > video');
     video.src = src;
     video.classList.remove('hide');
+    // a
+    const a = target.querySelector('placeholder > a');
+    a.classList.add('hide');
+    // btn : download (, skip)
+    const btn = target.querySelectorAll('button');
+    for (i=0; i<btn.length; i++) btn[i].classList.add('hide');
+    btn[0].classList.remove('hide');
 }
-function updateReady(targetId) {;}
-function updateProcess(targetId) {;}
-function updateDisabled(targetId) {;}
+
+function updateReady(target) {
+    // video
+    const video = target.querySelector('.placeholder > video');
+    video.classList.add('hide');
+    // a
+    const a = target.querySelector('placeholder > a');
+    a.classList.remove('hide');
+    // btn : download (, skip)
+    const btn = target.querySelectorAll('button');
+    for (i=0; i<btn.length; i++) btn[i].classList.remove('hide');
+    btn[0].classList.add('hide');
+}
+
+function updateProcess(target) {
+    // video
+    const video = target.querySelector('.placeholder > video');
+    video.classList.add('hide');
+    // a
+    const a = target.querySelector('placeholder > a');
+    a.classList.add('hide');
+    // btn : all
+    const btn = target.querySelectorAll('button');
+    for (i=0; i<btn.length; i++) btn[i].classList.remove('hide');
+
+    // # target.innerHTML로 스피너 추가하기
+
+    // # 하단 notification 블록 추가하기 : innerHTML, a 태그
+    const process = document.getElementById('process');
+    const note = document.createElement('div');
+    
+}
+
+function skip() {
+    const rmSilence = document.getElementById('rm-silence');
+    const addEffect = document.getElementById('add-effect');
+    rmSilence.classList.add('disabled');
+    addEffect.classList.remove('disabled');
+}
